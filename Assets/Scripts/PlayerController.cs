d4858f79c7e7cc3fd72f53724fefc8f8d4924b52
Aidan <33384177+PhyrexianAgent@users.noreply.github.com>
2022-12-03 14:35:21 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Move Values")]
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float jumpHeight = 5;
+
+    [Header("Ground Check Values")]
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Transform checkPos;
+
+    [Header("Component References")]
+    public Rigidbody2D rigid;
+    public Animator anim;
+    public SpriteRenderer spriteRender;
+
+    private bool onGround = true;
+    private bool jumped = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TestForKeys();
+        TestForGrounded();
+    }
+
+    private void FixedUpdate()
+    {
+        int moveDir = Convert.ToInt32(Input.GetKey(KeyCode.D)) - Convert.ToInt32(Input.GetKey(KeyCode.A));
+        rigid.velocity = new Vector2(moveDir * speed, rigid.velocity.y);
+        anim.SetBool("isMoving", moveDir != 0);
+        
+        if (moveDir != 0)
+        {
+            spriteRender.flipX = moveDir == -1;
+        }
+    }
+
+    private void TestForKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.velocity += new Vector2(0, jumpHeight);
+            jumped = true;
+            //anim.SetTrigger("Jump");
+        }
+    }
+
+    private void TestForGrounded()
+    {
+        bool grounded = Physics2D.OverlapCircle(checkPos.position, radius, groundMask);
+        if (grounded && !onGround)
+        {
+            anim.SetTrigger("Landed");
+        }
+        else if (!grounded && onGround)
+        {
+            //Debug.Log(jumped ? "Jump" : "Falling");
+            anim.SetTrigger(jumped ? "Jump" : "Falling");
+            jumped = false;
+        }
+        onGround = grounded;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Handles.color = Color.green;
+        //Handles.draw
+        /*Gizmos.color = Color.green;
+        Vector2 boxPos = (Vector2)transform.position + rectPosition;
+        //Gizmos.DrawCube(transform.position + (Vector3)rectPosition, rectVals);
+        Gizmos.DrawLine(boxPos + rectVals, boxPos + new Vector2(0, rectVals.y));
+        Gizmos.DrawLine(boxPos, boxPos + new Vector2(0, rectVals.y));
+        Gizmos.DrawLine(boxPos, boxPos + new Vector2(rectVals.x, 0));
+        Gizmos.DrawLine(boxPos + rectVals, boxPos + new Vector2(rectVals.x, 0));*/
+        Handles.DrawWireDisc(checkPos.position, Vector3.back, radius);
+    }
+}
